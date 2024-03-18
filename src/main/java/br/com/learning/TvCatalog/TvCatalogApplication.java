9073d5deaeca59f479bc80ed1992ff2a98eac7d3
Marcos Cunha <marcos.cunha1025@gmail.com>
2024-03-18 16:21:09 -0300

    Created project Spring Boot

---
+++ src/main/java/br/com/learning/TvCatalog/TvCatalogApplication.java
@@ -0,0 +1,13 @@
+package br.com.learning.TvCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvCatalogApplication.class, args);
+	}
+
+}
