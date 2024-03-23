e0bcdd26664e841a34bf7c148b7d61cabfb0b86d
Marcos Cunha <marcos.cunha1025@gmail.com>
2024-03-23 13:23:52 -0300

    Implemented JSF configuration

--- src/main/java/br/com/learning/TvCatalog/TvCatalogApplication.java
+++ src/main/java/br/com/learning/TvCatalog/TvCatalogApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TvCatalogApplication {
+public class TvCatalogApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TvCatalogApplication.class, args);
